726b0828d7efb8c9fa98ea48120d27bf6e5e6df7
Peter Mellett <pjm@petermellett.co.uk>
2014-11-06 16:56:24 +0000

    better

--- test/final-boss-cat.js
+++ test/final-boss-cat.js
@@ -1,31 +1,31 @@
-var cat = require('../final-boss-cat-es5.js'),
+var Cat = require('../final-boss-cat-es5.js').Cat,
     test = require('tape');
 
 test('sanity!', function(t) {
-  t.equals(typeof cat.Cat, 'function', 'cat is an object')
+  t.equals(typeof Cat, 'function', 'cat is an object')
   t.end();
 })
 
 test('cat is called simon by default', function(t) {
-  var c = new cat.Cat();
+  var c = new Cat();
   t.equals(c.name, 'Simon', 'cat is called simon')
   t.end();
 })
 
 test('cat can be named in the constructor', function(t) {
-  var c = new cat.Cat('Reginald');
+  var c = new Cat('Reginald');
   t.equals(c.name, 'Reginald', 'cat is called Reginald')
   t.end();
 })
 
 test('noisy animal', function(t) {
-  var c = new cat.Cat();
+  var c = new Cat();
   t.equals('MROW', c.meow(), 'cat meow');
   t.end();
 })
 
 test('sleepy animal', function(t) {
-  var c = new cat.Cat();
+  var c = new Cat();
 
 
   c.sleep(function(){
@@ -35,7 +35,7 @@ test('sleepy animal', function(t) {
 })
 
 test('cat eats noisily', function(t) {
-  var c = new cat.Cat('FatCat');
+  var c = new Cat('FatCat');
 
   t.equals(c.eat(), 'nom nom nom', 'noisy cat eating');
 })
