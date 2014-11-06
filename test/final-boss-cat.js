234adad7e66367ec27643375757d505951ad8dd8
Peter Mellett <pjm@petermellett.co.uk>
2014-11-06 16:53:14 +0000

    almost works and that

--- test/final-boss-cat.js
+++ test/final-boss-cat.js
@@ -1,42 +1,41 @@
-var cat = require('Cat'),
+var cat = require('../final-boss-cat-es5.js'),
     test = require('tape');
 
 test('sanity!', function(t) {
-  t.equals(typeof Cat, object, 'cat is an object')
+  t.equals(typeof cat.Cat, 'function', 'cat is an object')
   t.end();
 })
 
 test('cat is called simon by default', function(t) {
-  var c = new Cat();
+  var c = new cat.Cat();
   t.equals(c.name, 'Simon', 'cat is called simon')
   t.end();
 })
 
 test('cat can be named in the constructor', function(t) {
-  var c = new Cat('Reginald');
+  var c = new cat.Cat('Reginald');
   t.equals(c.name, 'Reginald', 'cat is called Reginald')
   t.end();
 })
 
 test('noisy animal', function(t) {
-  var c = new Cat();
-  t.equals('MEOW', c.meow(), 'cat meow');
+  var c = new cat.Cat();
+  t.equals('MROW', c.meow(), 'cat meow');
   t.end();
 })
 
 test('sleepy animal', function(t) {
-  var c = new Cat(),
-      testCallback = function(){
+  var c = new cat.Cat();
+
+
+  c.sleep(function(){
         t.pass('yay!');
         t.end();
-      };
-
-
-  c.sleep(testCallback);
+  });
 })
 
 test('cat eats noisily', function(t) {
-  var c = new Cat();
+  var c = new cat.Cat('FatCat');
 
   t.equals(c.eat(), 'nom nom nom', 'noisy cat eating');
 })
